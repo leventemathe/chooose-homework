605a7d621502f0beda52cfed7d29ed9a54fd4c60
Levente Máthé <mathe.levente@haloinvesting.com>
2023-05-27 16:05:42 +0200

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import { ChakraProvider } from '@chakra-ui/react';
+
+import App from './App';
+import theme from './theme';
+
+const container = document.getElementById('root');
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(container!);
+root.render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { extendTheme, ThemeConfig } from '@chakra-ui/react';
+
+// TODO
+const config: ThemeConfig = {};
+
+const theme = extendTheme({ config });
+
+export default theme;
