ac5994cdf4886f58892638fcfd47754c30e85136
Levente Máthé <mathe.levente@haloinvesting.com>
2023-05-29 12:06:09 +0200

    refine emission formatting

--- src/textFormatters/formatEmission.test.ts
+++ src/textFormatters/formatEmission.test.ts
@@ -2,22 +2,22 @@ import { formatEmission } from './formatEmission';
 
 describe('formatEmission', () => {
   it('should format emission in kg for small numbers', () => {
-    const result = formatEmission(500);
-    expect(result).toBe('500 kg');
+    const result = formatEmission(512);
+    expect(result).toBe('512 kg');
   });
 
   it('should format emission in t for big numbers', () => {
-    const result = formatEmission(1500);
-    expect(result).toBe('1.5 t');
+    const result = formatEmission(1523);
+    expect(result).toBe('1.52 t');
   });
 
   it('should format emission in kg for negative small numbers', () => {
-    const result = formatEmission(-500);
-    expect(result).toBe('-500 kg');
+    const result = formatEmission(-531);
+    expect(result).toBe('-531 kg');
   });
 
   it('should format emission in t for negative big numbers', () => {
-    const result = formatEmission(-1500);
-    expect(result).toBe('-1.5 t');
+    const result = formatEmission(-1492);
+    expect(result).toBe('-1.49 t');
   });
 });
--- src/textFormatters/formatEmission.ts
+++ src/textFormatters/formatEmission.ts
@@ -1,6 +1,6 @@
 export function formatEmission(emission: number) {
   const isBigNumber = Math.abs(emission) / 1000 >= 1;
-  const emissionNumber = isBigNumber ? emission / 1000 : emission;
+  const emissionNumber = isBigNumber ? (emission / 1000).toFixed(2) : emission;
   const emissionUnit = isBigNumber ? 't' : 'kg';
   const emissionText = `${emissionNumber} ${emissionUnit}`;
 
